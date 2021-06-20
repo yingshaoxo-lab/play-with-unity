253735fb5eff16b3b0817be73f2c4f4f6cd13c0f
yingshaoxo <yingshaoxo@gmail.com>
2021-06-20 19:00:10 +0800

    Add some awesome animations to our girl model

--- Assets/2.Magic World/Scripts/control_the_player.cs
+++ Assets/2.Magic World/Scripts/control_the_player.cs
@@ -7,23 +7,36 @@ public class control_the_player : MonoBehaviour
     private CharacterController characterController;
     public float movementSpeed, rotationSpeed, jumpSpeed, gravity;
     public Vector3 movementDirection = Vector3.zero;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 inputMovement = transform.forward * movementSpeed * Input.GetAxisRaw("Vertical");
+        var vertical_input =  Input.GetAxisRaw("Vertical");
+        var horizontal_input = Input.GetAxisRaw("Horizontal");
+
+        if (vertical_input > 0 || horizontal_input > 0) {
+            animator.Play("walking");
+        } else {
+            animator.Play("standing");
+        }
+
+        // code that handles the forward moving
+        Vector3 inputMovement = transform.forward * movementSpeed * vertical_input;
         characterController.Move(inputMovement * Time.deltaTime);
 
-        transform.Rotate(Vector3.up * Input.GetAxisRaw("Horizontal") * rotationSpeed);
+        // code that handles the rotation
+        transform.Rotate(Vector3.up * horizontal_input * rotationSpeed);
 
+        // code that handles the jump
         if (Input.GetKeyDown(KeyCode.Space)) {// && characterController.isGrounded) {
-            movementDirection.y += jumpSpeed;
-            Debug.Log("jumped");
+            movementDirection.y = jumpSpeed;
         }
         movementDirection.y -= gravity * Time.deltaTime;
         characterController.Move(movementDirection* Time.deltaTime);
